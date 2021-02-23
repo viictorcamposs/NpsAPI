7f1269d840631ed62ee0f5322253b674879cdaa1
viictorcamposs <campos.viictor@gmail.com>
2021-02-23 14:29:42 -0300

    update: refactoring project

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
+import 'reflect-metadata';
 import express from 'express';
+import router from './routes';
+import './database'
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: "Mensagem enviada com sucesso!" });
-});
-
-app.post('/', (request, response) => {
-  return response.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running!'));
